40a6cb351d0f9668d27810b0976797d1fb1993fb
Valeria Sell <valeria_sell@outlook.com>
2021-03-21 21:59:20 -0600

    reacomodo de funciones y creación de carpeta de datos

---
+++ sources/data.py
@@ -0,0 +1,7 @@
+sospechoso = ['mejor amigo/a', 'novio/a', 'vecino/a', 'mensajero/a', 'extranio/a', 'hermanastro/a', 'colega del trabajo']
+arma = ['pistola', 'cuchillo', 'machete', 'pala', 'bate', 'botella', 'tubo', 'cuerda']
+motivo = ['venganza', 'celos', 'dinero', 'accidente', 'drogas', 'robo']
+parteCuerpo = ['cabeza', 'pecho', 'abdomen', 'espalda', 'pierna', 'brazo']
+lugar = ['sala', 'comedor', 'banio', 'terraza', 'cuarto', 'garage', 'patio', 'balcon', 'cocina']
+
+grupo = [sospechoso, arma, motivo, parteCuerpo, lugar]
